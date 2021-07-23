b67eb13d2ccb0c15b2c2f658e6f52ac8f44d804b
Thiago Lisboa <atomicfeast@hotmail.com>
2021-07-23 13:17:34 -0300

    Fixes dropdown borders rgb

--- rainbow.js
+++ rainbow.js
@@ -1,8 +1,14 @@
 const evo = document.querySelector(".toggle")
 const logo = document.querySelector("#logo svg")
 const nav = document.querySelector("nav")
+const body = document.querySelector("body")
+const itemdrop = document.querySelectorAll(".nav__itemdrop")
 
 evo.addEventListener("click", () => {
   logo.classList.toggle("rgb")
   nav.classList.toggle("rgb")
+  body.classList.toggle("light")
+  
+  itemdrop[0].classList.toggle("rgb")
+  itemdrop[1].classList.toggle("rgb")
 })
