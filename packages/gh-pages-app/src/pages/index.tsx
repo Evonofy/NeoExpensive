74ca56c9c3fa29641aa96b35571103cdf57a6494
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-08-03 20:43:24 -0300

    feat(web): add next-seo

--- packages/gh-pages-app/src/pages/index.tsx
+++ packages/gh-pages-app/src/pages/index.tsx
@@ -1,8 +1,14 @@
 import { FC } from "react"
 
+import { NextSeo } from "next-seo"
+
 const Home: FC = () => {
   return (
     <>
+      <NextSeo
+        title="home"
+        description="Homepage of EvoExpensive | For you to buy everything"
+      />
       hello world
     </>
   )
