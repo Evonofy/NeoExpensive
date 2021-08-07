3f201e9aae427bbcfdcb30c536d66699e407245a
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-08-07 17:22:11 -0300

    fix(storybook): add dummy mdx documentation :tada:

--- packages/gh-pages-app/src/stories/Button.stories.tsx
+++ packages/gh-pages-app/src/stories/Button.stories.tsx
@@ -2,15 +2,58 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { withDesign } from 'storybook-addon-designs';
 
+import mdxDoc from './doc.mdx';
+
 import { Button } from './Button';
 
 export default {
   title: 'Dummy/Button',
   component: Button,
   argTypes: {
-    backgroundColor: { control: 'color' }
+    // Assigns the argTypes to the Colors category
+    backgroundColor: {
+      control: 'color',
+      table: {
+        category: 'Colors',
+        // Assigns the argTypes to a specific subcategory
+        subcategory: 'Button colors'
+      }
+    },
+    primary: {
+      table: {
+        category: 'Colors',
+        subcategory: 'Button style'
+      }
+    },
+    label: {
+      table: {
+        category: 'Text',
+        subcategory: 'Button contents'
+      }
+    },
+    // Assigns the argType to the Events category
+    onClick: {
+      table: {
+        category: 'Events',
+        subcategory: 'Button Events'
+      }
+    },
+    // Assigns the argType to the Sizes category
+    size: {
+      table: {
+        category: 'Sizes'
+      }
+    }
+  },
+  args: {
+    backgroundColor: '#001100',
+    primary: true
   },
   parameters: {
+    docsOnly: false,
+    docs: {
+      page: mdxDoc
+    },
     layout: 'centered',
     design: {
       type: 'figma',
