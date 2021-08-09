9d2a63b37e4f746b8569ffaaae277ddb7d336719
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-08-08 21:47:19 -0300

    fix(api): fix axios .env conflict

--- packages/api/src/services/api.ts
+++ packages/api/src/services/api.ts
@@ -1,5 +1,20 @@
+import { config } from 'dotenv';
+import { resolve } from 'path';
+
 import axios from 'axios';
 
+const isTest = process.env.NODE_ENV === 'testing';
+
+let path: string = '';
+
+isTest
+  ? (path = resolve(__dirname, '..', '..', '.env.test'))
+  : (path = resolve(__dirname, '..', '..', '.env'));
+
+config({ path });
+
+const baseURL = process.env.API_URL;
+
 export const api = axios.create({
-  baseURL: process.env.API_URL
+  baseURL
 });
--- packages/api/src/tests/index.spec.ts
+++ packages/api/src/tests/index.spec.ts
@@ -1,3 +1,7 @@
-it('should sum two number', () => {
+import { api } from '@src/services';
+
+it('should sum two number', async () => {
+  const { data } = await api.get('/users');
+  console.log(data);
   expect(1 + 1).toBe(2);
 });
