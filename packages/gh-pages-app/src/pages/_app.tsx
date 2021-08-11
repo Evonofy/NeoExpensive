d17ffa2b816342ea4f829672d959843abd622b71
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-08-11 14:33:30 -0300

    revert(desktop): `feat(desktop): add custom header for app` & add is-electron

--- packages/gh-pages-app/src/pages/_app.tsx
+++ packages/gh-pages-app/src/pages/_app.tsx
@@ -6,12 +6,17 @@ import { Theme } from '@contexts';
 import { init_i18n } from '@lib';
 import { Head } from '@components';
 
+import isElectron from 'is-electron';
+
 import './global.scss';
 
 const app: FC<AppProps> = ({ Component, pageProps }: AppProps): JSX.Element => {
   /** Initialize multi-language service */
   init_i18n();
 
+  if (isElectron()) {
+    console.log('render application header.');
+  }
   /** Initialize Theme context */
   return (
     <>
