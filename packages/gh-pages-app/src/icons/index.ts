945aaedc8db74ad9fc71490d79eb00e6c139218c
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-08-11 14:55:13 -0300

    fix(header): add window actions icons

--- packages/gh-pages-app/src/icons/index.ts
+++ packages/gh-pages-app/src/icons/index.ts
@@ -1,1 +1,4 @@
 export { SvgSolidNeoExpensive } from './SolidNeoExpensive';
+export { SVGSolidMaximize } from './SolidMaximize';
+export { SVGSolidClose } from './SolidClose';
+export { SVGSolidMinus } from './SolidMinus';
