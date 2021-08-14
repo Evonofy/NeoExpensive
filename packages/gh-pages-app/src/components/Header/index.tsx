452e92ca6b37123dca8a46b36b1e8cdd6b1aeaaa
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-08-13 23:52:00 -0300

    fix(web): make base header

--- packages/gh-pages-app/src/components/Header/index.tsx
+++ packages/gh-pages-app/src/components/Header/index.tsx
@@ -15,14 +15,38 @@ import styles from './Header.module.scss';
 export const Header: FC = () => {
   return (
     <header className={styles.container}>
-      <div id="menu"></div>
-      <SVGFacebook color="blue" strokeWidth="4px" width={70} />
-      <SVGInstagram color="purple" strokeWidth="5px" width={70} />
-      <SVGTwitter color="purple" strokeWidth="5px" width={70} />
-      <SVGNeoExpensive color="blue" />
-      <SVGSearch color="blue" />
-      <SVGUser />
-      <SVGCart />
+      <div id="menu" className={styles.menu}>
+        <div className="social">
+          <a href="#facebook">
+            <SVGFacebook />
+          </a>
+          <a href="#instagram">
+            <SVGInstagram />
+          </a>
+          <a href="#twitter">
+            <SVGTwitter />
+          </a>
+        </div>
+
+        <div className="logo">
+          <a href="#neo">
+            <SVGNeoExpensive />
+          </a>
+        </div>
+
+        <div className="user">
+          <a href="#search">
+            <SVGSearch />
+          </a>
+          <a href="#user">
+            <SVGUser />
+          </a>
+          <a href="#cart">
+            <SVGCart />
+          </a>
+        </div>
+      </div>
+      <nav></nav>
     </header>
   );
 };
