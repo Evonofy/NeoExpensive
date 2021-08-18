d53b93b703d26a4b2388fac9f681a88227a6c191
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-08-18 17:15:44 -0300

    fix(server): fix .env location

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,4 @@
-import dotenv from 'dotenv';
-import path from 'path';
-
-dotenv.config({
-  path: path.resolve(__dirname, '..', '.env')
-});
+import 'dotenv/config';
 
 /* importing an instance of the express app */
 import '@infra/http';
