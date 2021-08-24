cbab7122ead6e7d0294c6f403d62138cb44d8f3e
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-08-24 16:00:09 -0300

    fix(server): use UTC date time instead of ISO date strings

--- packages/server/src/domain/user/entities/user.ts
+++ packages/server/src/domain/user/entities/user.ts
@@ -1,6 +1,6 @@
 import { v4 as uuid } from 'uuid';
 
-import { hashSync } from 'bcrypt';
+import { hashSync, compare } from 'bcrypt';
 
 interface UserProps {
   name: string;
@@ -17,8 +17,8 @@ interface Options {
 export class User {
   public readonly id: string;
 
-  public readonly created_at: Date;
-  public updated_at: Date;
+  public readonly created_at: number;
+  public updated_at: number;
 
   public name: string;
   public email: string;
@@ -29,16 +29,17 @@ export class User {
 
     if (!id) {
       this.id = uuid();
+    } else {
+      this.id = id;
     }
 
     if (!created_at) {
-      this.created_at = new Date();
+      this.created_at = Number(new Date().getTime());
+    } else {
+      this.created_at = Number(created_at);
     }
 
-    this.id = id;
-    this.created_at = created_at;
-
-    this.updated_at = new Date();
+    this.updated_at = Number(new Date().getTime());
 
     if (isHashed) {
       this.password = props.password;
@@ -51,5 +52,11 @@ export class User {
   public isValidEmail(email: string) {
     const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regex.test(email);
+  }
+
+  public async comparePassword(password: string, hashedPassword: string) {
+    const comparePassword = await compare(password, hashedPassword);
+
+    return comparePassword;
   }
 }
