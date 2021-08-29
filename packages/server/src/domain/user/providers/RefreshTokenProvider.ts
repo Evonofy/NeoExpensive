d236f50dec08c7654df23424e5f0a7b80b60750d
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-08-28 21:02:26 -0300

    feat(server): add `refreshTokenRepository` as a dependency for `RefreshTokenProvider`

--- packages/server/src/domain/user/providers/RefreshTokenProvider.ts
+++ packages/server/src/domain/user/providers/RefreshTokenProvider.ts
@@ -1,18 +1,18 @@
 import { v4 as uuid } from 'uuid';
 import dayjs from 'dayjs';
 
-import { prisma } from '@infra/prisma';
+import { IRefreshTokenRepository } from '@user/repositories';
 
 export class RefreshTokenProvider {
+  constructor(private refreshTokenRepository: IRefreshTokenRepository) {}
+
   async execute(userId: string) {
     const expiresIn = dayjs().add(7, 'days').unix();
 
-    const refreshToken = await prisma.refreshToken.create({
-      data: {
-        id: uuid(),
-        userId,
-        expiresIn
-      }
+    const refreshToken = await this.refreshTokenRepository.create({
+      id: uuid(),
+      userId,
+      expiresIn
     });
 
     return { refreshToken };
