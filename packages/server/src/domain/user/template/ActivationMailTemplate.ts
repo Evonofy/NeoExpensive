3135ea359bae726898f232d5290974f96814ee21
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-08-30 15:35:17 -0300

    fix(email): use baseTemplate in Activation mail

--- packages/server/src/domain/user/template/ActivationMailTemplate.ts
+++ packages/server/src/domain/user/template/ActivationMailTemplate.ts
@@ -1,15 +1,22 @@
+import { BaseMailTemplate } from './MailTemplate';
+
 interface ActivationMailProps {
   name: string;
 }
 
 export const ActivationMailTemplate = ({ name }: ActivationMailProps) => {
-  return `
-    <div>
-      <h1>Welcome to Neo Expensive ${name}!</h1>
-      <p>We're so glad to see you finally arrived!</p>
-      <button>
-        <a href="${process.env.APP_URL}/welcome">go to the platform</a>
-      </button>
-    </div>
-  `;
+  return BaseMailTemplate(
+    `
+    <h1>Welcome ${name}!</h1>
+    <p>
+      We're so glad to see you finally arrived!
+    </p>
+    <button>
+      <a href="${process.env.APP_URL}/welcome">Go to the platform</a>
+    </button>
+  `,
+    {
+      icon: 'success'
+    }
+  );
 };
--- packages/server/src/domain/user/template/MailTemplate.ts
+++ packages/server/src/domain/user/template/MailTemplate.ts
@@ -28,7 +28,6 @@ export const BaseMailTemplate = (email: string, { icon }: iconsProps) => {
           `;
       break;
   }
-  console.log(process.env.APP_URL);
   return `
     <style>
       * {
