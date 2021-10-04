c194bf29a7e16469f0c05419642439647ddbb87a
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-10-04 12:00:00 -0600

    fix(next): remove commented lines from dropdown scripts

--- packages/next/src/components/Layout/Header/index.tsx
+++ packages/next/src/components/Layout/Header/index.tsx
@@ -10,9 +10,10 @@ import {
 
 import { useClamp } from '@hooks';
 import { Link } from '@components';
-import { Navbar, NavbarItem } from './Navbar';
-import { Dropdown } from './Dropdown';
 import { HeaderContainer, X } from '@styles/components/header';
+
+import { Navbar, NavbarItem } from './Navbar';
+import { Dropdown, DropdownScript } from './Dropdown';
 
 type HeaderProps = {
   rootFontSize: number;
@@ -20,154 +21,7 @@ type HeaderProps = {
 
 export const Header: FC<HeaderProps> = ({ rootFontSize }) => {
   useEffect(() => {
-    var menuItems = [].slice.call(document.querySelectorAll('.menu__item a')),
-      menuSubs: any = [].slice.call(
-        document.querySelectorAll('.dropdown-menu')
-      ),
-      selectedMenu = undefined,
-      subBg: any = document.querySelector('.dropdown__bg'),
-      subBgBtm: any = document.querySelector('.dropdown__bg-bottom'),
-      subCnt: any = document.querySelector('.dropdown__wrap'),
-      header = document.querySelector('header'),
-      dropdownContainer: any = document.querySelector('.dropdown'),
-      closeDropdownTimeout,
-      startCloseTimeout = function () {
-        closeDropdownTimeout = setTimeout(() => {
-          closeDropdown();
-        }, 200);
-      },
-      stopCloseTimeout = function () {
-        clearTimeout(closeDropdownTimeout);
-      },
-      openDropdown = function (el) {
-        dropdownContainer.classList.remove('hide');
-        dropdownContainer.classList.add('show');
-
-        console.log(el);
-        //- get menu ID
-        var menuId = el.getAttribute('data-sub');
-        //- get related sub menu
-        var menuSub: any = document.querySelector(
-          '.dropdown-menu[data-sub="' + menuId + '"]'
-        );
-        //- get menu sub content
-        var menuSubCnt = menuSub.querySelector('.dropdown-menu__content');
-        //- get bottom section of current sub
-        var menuSubBtm = menuSubCnt
-          .querySelector('.bottom-section')
-          .getBoundingClientRect();
-        //- get height of top section
-        var menuSubTop = menuSubCnt
-          .querySelector('.top-section')
-          .getBoundingClientRect();
-        //- get menu position
-        var menuMeta = el.getBoundingClientRect();
-        //- get sub menu position
-        var subMeta = menuSubCnt.getBoundingClientRect();
-
-        //- set selected menu
-        selectedMenu = menuId;
-
-        //- Remove active Menu
-        menuItems.forEach(el => el.classList.remove('active'));
-        //- Set current menu to active
-        el.classList.add('active');
-
-        //- Remove active sub menu
-        menuSubs.forEach(el => el.classList.remove('active'));
-        //- Set current menu to active
-        menuSub.classList.add('active');
-
-        const isSmall = window.innerWidth <= 1440;
-
-        const dropdown = {
-          opacity: 1,
-          left: isSmall
-            ? '50%'
-            : menuMeta.left - (subMeta.width / 2 - menuMeta.width / 2) + 'px',
-          transform: isSmall ? 'translateX(-50%)' : '',
-          width: `${subMeta.width}px`,
-          height: `${subMeta.height}px`
-        };
-
-        //- Set dropdown menu background style to match current submenu style
-        subBg.style.opacity = dropdown.opacity;
-        subBg.style.left = dropdown.left;
-        subBg.style.transform = dropdown.transform;
-        subBg.style.width = dropdown.width;
-        subBg.style.height = dropdown.height;
-
-        //- Set dropdown menu bottom section background position
-        subBgBtm.style.top = menuSubTop.height + 'px';
-
-        //- Set sub menu style
-        subCnt.style.opacity = dropdown.opacity;
-        subCnt.style.left = dropdown.left;
-        subCnt.style.transform = dropdown.transform;
-        subCnt.style.width = dropdown.width;
-        subCnt.style.height = dropdown.height;
-
-        //- Set current sub menu style
-        menuSub.style.opacity = 1;
-
-        header.classList.add('dropdown-active');
-      },
-      closeDropdown = function () {
-        //- Remove active class from all menu items
-        menuItems.forEach(el => el.classList.remove('active'));
-        //- Remove active class from all sub menus
-        menuSubs.forEach(el => {
-          el.classList.remove('active');
-          el.style.opacity = 0;
-        });
-        //- set sub menu background opacity
-        subBg.style.opacity = 0;
-        //- set arrow opacity
-        // subArr.style.opacity = 0;
-
-        // unset selected menu
-        selectedMenu = undefined;
-
-        header.classList.remove('dropdown-active');
-        dropdownContainer.classList.add('hide');
-        dropdownContainer.classList.remove('show');
-      };
-
-    //- Binding mouse event to each menu items
-    menuItems.forEach(el => {
-      //- mouse enter event
-      el.addEventListener('mouseenter', function () {
-        stopCloseTimeout();
-        openDropdown(this);
-      });
-
-      el.addEventListener('focus', function () {
-        stopCloseTimeout();
-        openDropdown(this);
-      });
-
-      // el.addEventListener('blur', () => {
-      //   startCloseTimeout();
-      // });
-
-      // //- mouse leave event
-      // el.addEventListener('mouseleave', () => {
-      //   startCloseTimeout();
-      // });
-    });
-
-    //- Binding mouse event to each sub menus
-    menuSubs.forEach(el => {
-      el.addEventListener('click', () => {
-        stopCloseTimeout();
-        console.log('a');
-      });
-      el.addEventListener('mouseenter', () => stopCloseTimeout(), false);
-      // el.addEventListener('mouseleave', () => startCloseTimeout(), false);
-    });
-
-    subBg.addEventListener('mouseenter', () => stopCloseTimeout());
-    // subBg.addEventListener('mouseleave', () => startCloseTimeout());
+    DropdownScript();
   }, []);
 
   return (
