6918efa3ba39f12d33a79ce7cda96c4a3f877a8f
VitorGouveia <vitorneves.gouveia10@gmail.com>
2021-10-26 20:19:51 -0300

    chore(desktop): remove desktop app

--- packages/desktop/src/styles/styled.d.ts
+++
@@ -1,9 +0,0 @@
-import 'styled-components'
-import { defaultTheme } from './theme'
-
-declare module 'styled-components' {
-  export interface DefaultTheme {
-    colors: typeof defaultTheme.colors
-    backgrounds: typeof defaultTheme.backgrounds
-  }
-}
