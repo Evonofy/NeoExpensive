412788dc0b84ef4cec1ff9e4cec4eb67b953309f
AtomicFeasT <atomic-feast@hotmail.com>
2021-11-04 20:11:46 -0300

    feat(_support.scss): 80%
    Former-commit-id: f6570a85677dcb66507938d4f22d0a4814e05dc4

--- packages/web/js/utils/openOnClick.js
+++ packages/web/js/utils/openOnClick.js
@@ -4,3 +4,17 @@ navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav--open');
 });
 
+var acc = document.getElementsByClassName("support--section--accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
