08d7164280d702a8aa6389db7a36ee836d5c0b5d
VitorGouveia <vitorneves.gouveia10@gmail.com>
2021-11-29 17:03:14 -0300

    feat(web): add fallback to useSelector hook
    Former-commit-id: 35156eb5c8130698c5b5ca7c51a4e0583541a02d

--- packages/web/js/hooks/useSelector.js
+++ packages/web/js/hooks/useSelector.js
@@ -1,3 +1,26 @@
 export const useSelector = (selector) => {
+  const isClass = !!selector.split(".")[1];
+  const isID = !!selector.split("#")[1];
+
+  if (!isClass && !isID) {
+    console.log("theres no selector!!!!!");
+    /* get the selector string and search for it in the DOM */
+
+    const classSelectorElement = document.querySelector(`.${selector}`);
+    const IDSelectorElement = document.querySelector(`#${selector}`);
+
+    if (!!classSelectorElement) {
+      console.log("found a class!!!");
+
+      return classSelectorElement;
+    }
+
+    if (!!IDSelectorElement) {
+      console.log("found an ID!!!");
+
+      IDSelectorElement;
+    }
+  }
+
   return document.querySelector(selector);
 };
