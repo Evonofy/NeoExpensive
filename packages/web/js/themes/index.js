d8e1711c1926f9a2e7c99e9f889ccad94bf9f4e9
VitorGouveia <vitorneves.gouveia10@gmail.com>
2021-11-29 17:11:39 -0300

    feat(web): add theme switcher functionality with pure js

--- packages/web/js/themes/index.js
+++ packages/web/js/themes/index.js
@@ -1,47 +1,49 @@
 export class Theme {
-  /** Current theme */ 
-  theme
+  /** Current theme */
+  theme;
 
-  /** Theme that will be used as callback */ 
-  defaultTheme = "dark"
+  /** Theme that will be used as callback */
+  defaultTheme = "dark";
 
-  /** List of all the themes available */ 
+  /** List of all the themes available */
   availableThemes = {
     light: "light",
     dark: "dark",
-    rgb: "rgb"
-  }
+    rgb: "rgb",
+  };
 
   constructor(theme) {
-    let newTheme = theme
+    let newTheme = theme;
 
     /** If the theme isn't on the available themes list with pattern matching */
-    if(!this.availableThemes[theme]) {
-      newTheme = this.defaultTheme
+    if (!this.availableThemes[theme]) {
+      newTheme = this.defaultTheme;
     }
-    
-    this.setTheme(newTheme)
+
+    this.setTheme(newTheme);
   }
-  
+
   /** Just set theme in localStorage and body */
   setTheme(theme) {
-    this.theme = theme
+    this.theme = theme;
 
-    localStorage.setItem("theme", theme)
-    document.body.setAttribute("data-theme", theme)
+    localStorage.setItem("theme", theme);
+    document.body.setAttribute("data-theme", theme);
   }
 
   /** This will cycle through all themes */
-  cycleTheme() {
-    const themeArray = Object.keys(this.availableThemes)
+  cycle() {
+    const themeArray = Object.keys(this.availableThemes);
 
-    const current = themeArray.indexOf(document.body.getAttribute("data-theme"))
-    const max = themeArray.length
-    let next = current + 1
+    const current = themeArray.indexOf(
+      document.body.getAttribute("data-theme")
+    );
+    const max = themeArray.length;
+    let next = current + 1;
 
     /** If next item reaches the max of the array, set it to the first array item */
-    if(next === max) next = 0
+    if (next === max) next = 0;
 
-    this.setTheme(themeArray[next])
+    this.setTheme(themeArray[next]);
   }
 }
