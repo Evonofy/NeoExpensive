91c9c3b43dd00655ac66d4485dc127bd9d5ebad1
VitorGouveia <vitorneves.gouveia10@gmail.com>
2021-11-30 20:50:14 -0300

    feat(dashboard): add PWA feature to dashboard with multi-theme manifest.json
    Former-commit-id: 53168f4b77267a36f224fc80e41489b14ca1e561
    Former-commit-id: af627e96ca88d09cf0f36141e11e1a44f0a0a24a

--- packages/dashboard/.next/static/webpack/webpack.7b766c8f0b624903.hot-update.js
+++ packages/dashboard/.next/static/webpack/webpack.7b766c8f0b624903.hot-update.js
@@ -23,7 +23,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "5770926421459ac4"; }
+/******/ 	__webpack_require__.h = function() { return "0004843e6735d31c"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/harmony module decorator */
@@ -40,6 +40,19 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 		return module;
 /******/ 	};
 /******/ }();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ 
+/******/ // noop fns to prevent runtime errors during initialization
+/******/ if (typeof self !== "undefined") {
+/******/ 	self.$RefreshReg$ = function () {};
+/******/ 	self.$RefreshSig$ = function () {
+/******/ 		return function (type) {
+/******/ 			return type;
+/******/ 		};
+/******/ 	};
+/******/ }
 /******/ 
 /******/ }
 );
