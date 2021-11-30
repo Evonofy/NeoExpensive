e4df8bfb9a67b8dfdc213d29e575c12fdb788b32
VitorGouveia <vitorneves.gouveia10@gmail.com>
2021-11-30 20:17:30 -0300

    chore(web): remove comments from modal.js
    Former-commit-id: 75a8ffae6e599e0d7d915fedc7de6c78e4924aa5

--- packages/web/js/functions/modal.js
+++ packages/web/js/functions/modal.js
@@ -1,58 +1,5 @@
 import { useSelector } from '../hooks/useSelector.js';
 
-// class Modal {
-//   openModalButtons = useSelector('.support--card--button', {
-//     querySelectorAll: true,
-//   });
-//   modal;
-//   closeModalButtons = useSelector('.closeAccordion', {
-//     querySelectorAll: true,
-//   });
-
-//   window = window;
-//   overlay = useSelector('.overlay');
-
-//   constructor() {
-//     this.openModalButtons.forEach((button) => {
-//       this.modal = button.nextElementSibling;
-//       this.onClick(button, () => this.showModal(this.modal));
-//     });
-
-//     this.closeModalButtons.forEach((button) => {
-//       this.onClick(button, () =>
-//         this.hideModal(button.parentElement.parentElement)
-//       );
-//     });
-
-//     setTimeout(() => {
-//       this.window.onclick = ({ target }) => {
-//         if (
-//           target !== this.modal ||
-//           target !== this.modal.querySelector('article')
-//         ) {
-//           this.hideModal(this.modal);
-//           this.window.removeEventListener('click', () => {});
-//         }
-//       };
-//     }, 1000);
-//   }
-
-//   onClick = (element, callback) => {
-//     element.onclick = () => callback();
-//   };
-
-//   showModal(modal) {
-//     this.overlay.style.display = 'block';
-//     modal.classList.add('show');
-//   }
-
-//   hideModal(modal) {
-//     this.overlay.style.display = 'none';
-//     modal.classList.remove('show');
-//   }
-// }
-
-// new Modal();
 const allModalWrapperButtons = useSelector('.support--card--button', {
   querySelectorAll: true,
 });
@@ -93,39 +40,3 @@ allModalWrapperButtons.forEach((button) => {
     };
   };
 });
-
-// const modalWrapper = document.querySelectorAll('.modalWrapper');
-// const openAcc = document.querySelector('.openAccordion');
-// const openAccPedidos = document.querySelector('.openAccordionPedidos');
-// const openAccGarantia = document.querySelector('.openAccordionGarantia');
-// const openAccEntregas = document.querySelector('.openAccordionEntregas');
-// const openAccPagamentos = document.querySelector('.openAccordionPagamentos');
-
-// const modalWrapperPedidos = document.querySelector('.modalWrapperPedidos');
-// const modalWrapperGarantia = document.querySelector('.modalWrapperGarantia');
-// const modalWrapperEntregas = document.querySelector('.modalWrapperEntregas');
-// const modalWrapperPagamentos = document.querySelector(
-//   '.modalWrapperPagamentos'
-// );
-// const closeAcc = document.querySelectorAll('.closeAccordion');
-
-// openAccPedidos.addEventListener('click', function () {
-//   modalWrapperPedidos.classList.add('show');
-// });
-// openAccGarantia.addEventListener('click', function () {
-//   modalWrapperGarantia.classList.add('show');
-// });
-// openAccEntregas.addEventListener('click', function () {
-//   modalWrapperEntregas.classList.add('show');
-// });
-// openAccPagamentos.addEventListener('click', function () {
-//   modalWrapperPagamentos.classList.add('show');
-// });
-
-// closeAcc.forEach((closeAcc) => {
-//   closeAcc.addEventListener('click', function () {
-//     modalWrapper.forEach((modalWrapper) => {
-//       modalWrapper.classList.remove('show');
-//     });
-//   });
-// });
