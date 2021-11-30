5b56c0c8af7bfd91661ddf026d8b463385163f66
VitorGouveia <vitorneves.gouveia10@gmail.com>
2021-11-30 15:53:29 -0300

    feat(web): :tada: add google translation to neo-expensive
    Former-commit-id: a6c6aa96c62e9e2700e23f7964a8bae551f56397
    Former-commit-id: d1b5a0e3ba6b4305f44f23085917dc1374491ff6

--- packages/web/js/hooks/useSelector.js
+++ packages/web/js/hooks/useSelector.js
@@ -1,26 +1,26 @@
-export const useSelector = (selector, all) => {
-  const $ = !all
+export const useSelector = (selector, { querySelectorAll = false } = {}) => {
+  const $ = !querySelectorAll
     ? document.querySelector.bind(document)
     : document.querySelectorAll.bind(document);
 
-  const isClass = !!selector.split(".")[1];
-  const isID = !!selector.split("#")[1];
+  const isClass = !!selector.split('.')[1];
+  const isID = !!selector.split('#')[1];
 
   if (!isClass && !isID) {
-    console.log("theres no selector!!!!!");
+    console.log('theres no selector!!!!!');
     /* get the selector string and search for it in the DOM */
 
     const classSelectorElement = $(`.${selector}`);
     const IDSelectorElement = $(`#${selector}`);
 
     if (!!classSelectorElement) {
-      console.log("found a class!!!");
+      console.log('found a class!!!');
 
       return classSelectorElement;
     }
 
     if (!!IDSelectorElement) {
-      console.log("found an ID!!!");
+      console.log('found an ID!!!');
 
       IDSelectorElement;
     }
