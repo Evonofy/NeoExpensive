6b6ec29afb8c140f1d79ec75ed68aa71893722e6
VitorGouveia <vitorneves.gouveia10@gmail.com>
2021-12-01 14:36:58 -0300

    fix(dashboard): fix HUNTING next.js css bug + add Link components

--- packages/dashboard/src/components/Header/index.tsx
+++ packages/dashboard/src/components/Header/index.tsx
@@ -1,34 +1,56 @@
 import { FC } from "react";
 import Image, { ImageProps } from "next/image";
 
-export const NavItem: FC<ImageProps> = ({ src, children }) => {
+import { A } from "@components";
+
+import styles from "@neo/wunderlust/dist/dashboard/components/header.module.css";
+
+interface NavItemProps extends ImageProps {
+  href: string;
+}
+
+export const NavItem: FC<NavItemProps> = ({ src, children, href }) => {
+  const pageName = href.split("/")[1];
+
   return (
     <li>
-      <Image width={32} height={32} src={src} />
+      <A href={href} name={`Go to ${pageName} page`}>
+        <Image width={32} height={32} src={src} />
 
-      <h1>
-        <strong>{children}</strong>
-      </h1>
+        <h1>
+          <strong>{children}</strong>
+        </h1>
+      </A>
     </li>
   );
 };
 
 export const Header: FC = () => {
   return (
-    <header>
+    <header className={styles.header}>
       <section className="title">
-        <Image src={"/logo.svg"} width={60} height={60} />
-        <h1>
-          <strong>Neo Expensive</strong>
-        </h1>
+        <A href="/" name="Go to Homepage">
+          <Image src={"/logo.svg"} width={60} height={60} />
+          <h1>
+            <strong>Neo Expensive</strong>
+          </h1>
+        </A>
       </section>
 
       <nav>
-        <ul>
-          <NavItem src={"/cube.svg"}>Overview</NavItem>
-          <NavItem src={"/paper.svg"}>Products</NavItem>
-          <NavItem src={"/user.svg"}>Clients</NavItem>
-          <NavItem src={"/at.svg"}>E-mail</NavItem>
+        <ul role="list">
+          <NavItem href="/overview" src={"/cube.svg"}>
+            Overview
+          </NavItem>
+          <NavItem href="/products" src={"/paper.svg"}>
+            Products
+          </NavItem>
+          <NavItem href="/clients" src={"/user.svg"}>
+            Clients
+          </NavItem>
+          <NavItem href="/email" src={"/at.svg"}>
+            E-mail
+          </NavItem>
         </ul>
       </nav>
     </header>
--- packages/dashboard/src/components/index.ts
+++ packages/dashboard/src/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./Layout";
 export * from "./Link";
 export * from "./seo";
+export * from "./Redirect";
