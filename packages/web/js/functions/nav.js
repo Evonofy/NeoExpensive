261d912d51197862e921b65c3ef0f3b24620d395
VitorGouveia <vitorneves.gouveia10@gmail.com>
2021-12-06 03:41:19 -0300

    refactor(nav): mode nav code from `functions` to `lib`

--- packages/web/js/functions/nav.js
+++
@@ -1,7 +0,0 @@
-import { useSelector } from '../hooks/useSelector.js';
-
-const navToggle = useSelector('.hamburger--header--button');
-
-navToggle.onclick = () => {
-  document.body.classList.toggle('nav--open');
-};
