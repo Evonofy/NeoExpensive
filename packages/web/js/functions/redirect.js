24a681f346c23b1cb1431828bcef8978e966a2ed
VitorGouveia <vitorneves.gouveia10@gmail.com>
2021-12-06 03:50:11 -0300

    feat(register): create `useCookie` hook + redirect fn + finish register connection with API
    Former-commit-id: bc245f87c4e197838efaa38fb120f9dc0e03d7c4
    Former-commit-id: 4be99e88cec28dea3f99e25cba8c4a13e82bbfed

---
+++ packages/web/js/functions/redirect.js
@@ -0,0 +1,13 @@
+export const redirect = (path) => {
+  const isLocalhost = window.location.host === '127.0.0.1:5500';
+
+  const pathPrefix = isLocalhost
+    ? 'packages/web'
+    : 'neo-expensive/packages/web';
+
+  const host = isLocalhost
+    ? 'http://127.0.0.1:5500'
+    : 'https://esquemaflorescer.github.io';
+
+  window.location.href = `${host}/${pathPrefix}/${path}`;
+};
