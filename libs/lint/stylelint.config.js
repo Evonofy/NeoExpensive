f5e3f0c680bf8d05fb804e96289f91c99117d2e4
VitorGouveia <vitorneves.gouveia10@gmail.com>
2022-02-14 20:30:02 -0300

    :sparkles: feat(styles): add & setup Stylelint + PostCSS
    Former-commit-id: fa97f1a6fedba4185d05e9a6e12732a95e9d36de
    Former-commit-id: 0fe102bf37aff486b3971f27095a4b2f6332aa44

---
+++ libs/lint/stylelint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-prettier",
+    "stylelint-config-standard-scss",
+  ],
+  rules: {
+    "color-no-invalid-hex": true,
+  },
+};
