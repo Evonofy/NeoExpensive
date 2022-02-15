6eb98365a5df3e8b4cef3c214f394d1392b77d62
VitorGouveia <vitorneves.gouveia10@gmail.com>
2022-02-15 14:09:51 -0300

    :sparkles: feat(@neo/lint): setup eslint + prettier

---
+++ .prettierrc.js
@@ -0,0 +1,3 @@
+const configuration = require('@neo/lint/prettier.config.js');
+
+module.exports = configuration;
---
+++ libs/lint/prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: true,
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  trailingComma: 'es5',
+};
