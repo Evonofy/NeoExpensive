1e3c41a0d9ecf5ab33e18ff3f580aa4a3d324f8f
VitorGouveia <vitorneves.gouveia10@gmail.com>
2022-02-16 05:10:06 -0300

    :lipstick: style(@neo/postcss): add cssnano and postcss-font-magician plugins
    Former-commit-id: 81a2797221005fd92fecf94ff9c8d7c4cc19f488

--- packages/global/postcss.config.js
+++ packages/global/postcss.config.js
@@ -2,4 +2,20 @@ const { configuration } = require('@neo/postcss');
 
 module.exports = configuration({
   customSyntax: true,
+  plugins: [
+    require('postcss-font-magician')(),
+    // custom: {
+    //   Inter: {
+    //     variants: {
+    //       normal: {
+    //         400: {
+    //           url: {
+    //             woff2: 'path/to/my-body-font-normal-400.woff2',
+    //           },
+    //         },
+    //       },
+    //     },
+    //   },
+    // },
+  ],
 });
