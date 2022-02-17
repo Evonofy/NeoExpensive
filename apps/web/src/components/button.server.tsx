110b7e840d2ad0fc83a82c019085ddf0107361bf
VitorGouveia <vitorneves.gouveia10@gmail.com>
2022-02-17 03:29:17 -0300

    :bug: fix(web): remove experimental next.js since it breaks export
    Former-commit-id: b557aa17f30cb8a647d6651d5277702c24688130
    Former-commit-id: 09cabc7f60c192cdbda92c1a13b9adc5e4d54b4a

--- apps/web/src/components/button.server.tsx
+++
@@ -1,5 +0,0 @@
-import type { FC } from 'react';
-
-export const Button: FC = () => {
-  return <button>hello world</button>;
-};
--- apps/web/src/lib/confetti.ts
+++
@@ -1,5 +0,0 @@
-// import CanvasConfetti from 'canvas-confetti';
-// import confettiCDN from 'https://cdn.skypack.dev/canvas-confetti';
-
-// export const confetti = confettiCDN as typeof CanvasConfetti;
-export const confetti = 'confettiCDN as typeof CanvasConfetti;';
