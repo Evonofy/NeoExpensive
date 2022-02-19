0b4b5cd5ed0bdc61eee4265f134079064c3c9fe2
VitorGouveia <vitorneves.gouveia10@gmail.com>
2022-02-19 19:11:02 -0300

    :white_check_mark: test(login/register): add login feature + register tests

--- services/users/application/repositories/UsersRepository.ts
+++ services/users/application/repositories/UsersRepository.ts
@@ -2,4 +2,6 @@ import { User } from '../../domain/entities/user';
 
 export interface UsersRepository {
   findByEmail(id: string): Promise<User | null>;
+  store({ user }: { user: User }): Promise<void>;
+  clean(): Promise<void>;
 }
--- services/users/application/use-cases/register-user/index.spec.ts
+++ services/users/application/use-cases/register-user/index.spec.ts
@@ -1,17 +1,42 @@
 import { RegisterUser } from '.';
 import { InMemoryUsersRepository } from '../../repositories/drivers/tests/in-memory-users-repository';
 
+const usersRepository = new InMemoryUsersRepository();
+
+const registerUserTestFactory = () => {
+  const registerUser = new RegisterUser(usersRepository);
+
+  return {
+    registerUser,
+  };
+};
+
 describe('Register User', () => {
   it('should be able to create a new user', async () => {
-    const usersRepository = new InMemoryUsersRepository();
-    const registerUser = new RegisterUser(usersRepository);
+    const { registerUser } = registerUserTestFactory();
 
     const sut = await registerUser.execute({
       name: 'test',
       email: 'test@test.com',
+      password: '123',
     });
 
     expect(sut.user.props.name).toBe('test');
     expect(sut.user.props.email).toBe('test@test.com');
+  });
+
+  it('should not be able to create user with same e-mail', async () => {
+    const { registerUser } = registerUserTestFactory();
+
+    try {
+      await registerUser.execute({
+        name: 'test',
+        email: 'test@test.com',
+        password: '123',
+      });
+    } catch (err) {
+      const error = err as Error;
+      expect(error.message).toBe('User already exists.');
+    }
   });
 });
