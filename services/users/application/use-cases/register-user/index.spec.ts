06ef1881142452309d2e5b69932fa93158b2d28b
VitorGouveia <vitorneves.gouveia10@gmail.com>
2022-02-19 22:25:38 -0300

    :bug: fix(@neo/users): fix in-memory-users-repository not comparing email in findByEmail method

--- services/users/application/use-cases/register-user/index.spec.ts
+++ services/users/application/use-cases/register-user/index.spec.ts
@@ -3,6 +3,10 @@ import { InMemoryUsersRepository } from '../../repositories/drivers/tests/in-mem
 
 const usersRepository = new InMemoryUsersRepository();
 
+beforeEach(async () => {
+  await usersRepository.clean();
+});
+
 const registerUserTestFactory = () => {
   const registerUser = new RegisterUser(usersRepository);
 
@@ -28,6 +32,12 @@ describe('Register User', () => {
   it('should not be able to create user with same e-mail', async () => {
     const { registerUser } = registerUserTestFactory();
 
+    await registerUser.execute({
+      name: 'test',
+      email: 'test@test.com',
+      password: '123',
+    });
+
     try {
       await registerUser.execute({
         name: 'test',
@@ -38,5 +48,39 @@ describe('Register User', () => {
       const error = err as Error;
       expect(error.message).toBe('User already exists.');
     }
+  });
+
+  it('should be able to automatically assign createdAt to new user', async () => {
+    const { registerUser } = registerUserTestFactory();
+
+    const createdAt = new Date();
+
+    const sut = await registerUser.execute({
+      name: 'test',
+      email: 'test@test.com',
+      password: '123',
+      createdAt,
+    });
+
+    expect(sut.user.props.name).toBe('test');
+    expect(sut.user.props.email).toBe('test@test.com');
+    expect(sut.user.props.createdAt).toBe(createdAt);
+  });
+
+  it('should be able to automatically assign updatedAt to new user', async () => {
+    const { registerUser } = registerUserTestFactory();
+
+    const updatedAt = new Date();
+
+    const sut = await registerUser.execute({
+      name: 'test',
+      email: 'test@test.com',
+      password: '123',
+      updatedAt,
+    });
+
+    expect(sut.user.props.name).toBe('test');
+    expect(sut.user.props.email).toBe('test@test.com');
+    expect(sut.user.props.updatedAt).toBe(updatedAt);
   });
 });
