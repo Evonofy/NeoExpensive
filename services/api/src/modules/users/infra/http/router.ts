a659b3ac57dc2435a2f7408e6a392fd398723c24
VitorGouveia <vitorneves.gouveia10@gmail.com>
2022-02-20 21:50:36 -0300

    :sparkles: feat(@neo/api): add rest implementation of register user use case
    Former-commit-id: 7638d2c34fc65887e29b7af38c7a96f7991b0f36

--- services/api/src/modules/users/infra/http/router.ts
+++ services/api/src/modules/users/infra/http/router.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 
-import { LoginUserController } from '../../use-cases';
+import { LoginUserController, RegisterUserController } from '../../use-cases';
 
 // eslint-disable-next-line new-cap
 export const usersRouter = express.Router();
 
 usersRouter.post('/login', LoginUserController);
+usersRouter.post('/register', RegisterUserController);
