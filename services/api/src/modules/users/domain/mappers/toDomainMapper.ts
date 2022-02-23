4a804b1875b2082c1a8b6af8d4df7ed268119252
VitorGouveia <vitorneves.gouveia10@gmail.com>
2022-02-23 14:40:15 -0300

    :sparkles: feat: add github oauth login

--- services/api/src/modules/users/domain/mappers/toDomainMapper.ts
+++ services/api/src/modules/users/domain/mappers/toDomainMapper.ts
@@ -5,6 +5,7 @@ export function toDomainMapper(user: UserDTO): User {
   // prettier-ignore
   const domainUser = User.create({
     ...user,
+    password: user.password || "",
   }, user.id);
 
   return domainUser;
