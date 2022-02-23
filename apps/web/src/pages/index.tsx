8ac80ee9d270e0faea75efd509c3de9c0245bad9
VitorGouveia <vitorneves.gouveia10@gmail.com>
2022-02-23 11:55:46 -0300

    :bug: fix(merge): fix merge conflict with main

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -11,26 +11,37 @@ const Home: NextPage = () => {
   return (
     <main>
       <h1>main page</h1>
-      {user && <h2 style={{ color: 'red' }}>Hello {user.name}</h2>}
-      <div>
-        <Link href="/login">
-          <a>Go to login</a>
-        </Link>
-      </div>
-      <div>
-        <Link href="/register">
-          <a>Go to register</a>
-        </Link>
-      </div>
-      <div>
-        <Link href="/forgot-password">
-          <a>forgot password</a>
-        </Link>
-      </div>
+      {!user && (
+        <div>
+          <div>
+            <Link href="/login">
+              <a>Go to login</a>
+            </Link>
+          </div>
+          <div>
+            <Link href="/register">
+              <a>Go to register</a>
+            </Link>
+          </div>
+          <div>
+            <Link href="/forgot-password">
+              <a>forgot password</a>
+            </Link>
+          </div>
+          <div>
+            <Link href={`${process.env.NEXT_PUBLIC_API_URL}/users/login/oauth/github`}>
+              <a>login with github</a>
+            </Link>
+          </div>
+        </div>
+      )}
       {user && (
-        <a>
-          <button onClick={() => logout()}>logout</button>
-        </a>
+        <div>
+          <h2 style={{ color: 'red' }}>Hello {user.name}</h2>
+          <a>
+            <button onClick={() => logout()}>logout</button>
+          </a>
+        </div>
       )}
       <Icon />
     </main>
--- services/api/src/@types/env.d.ts
+++ services/api/src/@types/env.d.ts
@@ -12,6 +12,8 @@ declare global {
       MAIL_SENDER_EMAIL: string;
       ACCESS_TOKEN_SECRET: string;
       REFRESH_TOKEN_SECRET: string;
+      GITHUB_CLIENT_ID: string;
+      GITHUB_CLIENT_SECRET: string;
     }
   }
 }
